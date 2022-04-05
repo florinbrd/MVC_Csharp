fd705e193a60c5e5b860d453c51feb0105e7a6e0
Florin Leonard Bordei <43433262+florinbrd@users.noreply.github.com>
2022-04-05 10:18:27 +0200

    done.

---
+++ MVC_project/MVC_project/Controllers/CategoryController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_project.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVC_project.Models;
+
+
+namespace MVC_project.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    
+
+    //GET
+    public IActionResult Edit(int? id)
+    {
+       if(id==null || id == 0)
+       {
+        return NotFound();
+       }
+
+       var CategoryFromDb = _db.Categories.Find(id);
+
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(CategoryFromDb);
+    }
+
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name.");
+        }
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+
+
+     //GET
+    public IActionResult Delete(int? id)
+    {
+    if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+    var CategoryFromDb = _db.Categories.Find(id);
+
+    if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+    return View(CategoryFromDb);
+        }
+
+        //DELETE
+        [HttpDelete]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.Find(id); 
+            if(obj==null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges(); 
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
